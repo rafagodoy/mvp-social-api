a8b91814425afaa54b5dac6a3f1900dd59359e2b
Rafael Godoy <rafael.godoy@fotop.com.br>
2020-07-08 23:27:37 -0300

    inclusão de novas rotas REST

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Users from "../controllers/Users";
 import UsersBankAccounts from "../controllers/UsersBankAccounts";
 import UsersAddresses from "../controllers/UsersAddresses";
 import Donations from "../controllers/Donations";
+import Acknowledgments from "../controllers/Acknowledgments";
 import Passwords from "../controllers/Passwords";
 import Session from "../controllers/Session";
 import authMiddleware from "../middlewares/authMiddleware";
@@ -33,8 +34,10 @@ router.get("/users/:idUsers/addresses/:idUsersAddresses", authMiddleware, UsersA
 
 router.post("/users/:id/donations", authMiddleware, Donations.create);
 
-router.get("/users/:id/donations", authMiddleware, Donations.view);
+router.get("/users/:id/donations/:status", authMiddleware, Donations.view);
 
-router.patch("/donations/:id", authMiddleware, Donations.update);
+router.patch("/donations/:id/:status", authMiddleware, Donations.update);
+
+router.patch("/users/:idUsers/donations/:idDonations/to-thank", authMiddleware, Acknowledgments.update);
 
 export default router;
