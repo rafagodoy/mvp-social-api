fa3440cff001c0f5b42e83b32d365199b8f0e54d
Rafael Godoy <rafael.godoy@fotop.com.br>
2020-07-08 23:24:57 -0300

    set de timezone

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,6 +4,7 @@ const Op = Sequelize.Op;
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    timezone: "-03:00",
     dialect: process.env.DB_DIALECT,
     pool: {
         max: 10,
